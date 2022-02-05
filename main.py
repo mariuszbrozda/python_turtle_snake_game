bb27754adcbb13c6ec4b8f95b73f643e45cf4872
Mariusz <mariuszbrozda.dublin@gmail.com>
2022-02-05 17:20:56 +0000

    Create loop to move snake constantly forward

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 from turtle import Turtle, Screen
 
 screen = Screen()
+screen.listen()
 screen.setup(600, 600)
 screen.bgcolor('grey')
 screen.title('Snake')
@@ -19,18 +21,21 @@ for position in starting_positions:
     segment.color('white')
     segment.penup()
     segment.goto(position)
+    segments.append(segment)
+
 
 
 game_is_on = True
 
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
 
-
-
-
-
+    for seq_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seq_num - 1].xcor()
+        new_y = segments[seq_num - 1].ycor()
+        segments[seq_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 
 
